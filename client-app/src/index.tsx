29fc726d26d2a8c396a2ccad150e3905839356c8
haruna <haruna@writemedia.co.uk>
2022-11-04 13:41:17 -1200

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/layout/App";
 import "./app/layout/style.css";
+import { store, StoreContext } from "./app/stores/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>
+);
